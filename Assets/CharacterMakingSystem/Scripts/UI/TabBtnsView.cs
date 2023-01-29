d6b2edded331744f194613769cde001bf5a3ca0f
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2023-01-29 23:50:31 +0900

    タブボタン周りを実装

--- Assets/CharacterMakingSystem/Scripts/UI/TabBtnsView.cs
+++ Assets/CharacterMakingSystem/Scripts/UI/TabBtnsView.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,13 +12,19 @@ namespace CharacterMakingSystem.UI
     /// </summary>
     public class TabBtnsView : MonoBehaviour
     {
+        [SerializeField, Tooltip("スクロールバーの消えていくアニメーションの所要時間")]
+        private float animDuration = 0.5f;
+        
         [SerializeField, Tooltip("タブボタン")]
         private TabView[] tabViews = null;
+        
+        [SerializeField, Tooltip("タブラインバー")]
+        private Scrollbar tabLineBar = null;
 
         /// <summary>
         /// ボタンが押されていないときの色
         /// </summary>
-        private static readonly Color disableColor = new Color(1, 1, 1, 0.7f);
+        private static readonly Color DISABLE_COLOR = new Color(1, 1, 1, 0.7f);
 
         /// <summary>
         /// タブボタンとタブそのものを一括で扱う
@@ -34,37 +39,63 @@ namespace CharacterMakingSystem.UI
         }
         
         // Start is called before the first frame update
-        void Start()
+        private void Start()
         {
             if(tabViews.Length <= 1) return;
+
+            // ボタンの配置域と最小座標
+            var btnsRect = tabViews[0].btn.transform.parent.GetComponent<RectTransform>();
+            var btnsSize = btnsRect.rect.size.x * btnsRect.lossyScale.x;
+            var btnRect = tabViews[0].btn.GetComponent<RectTransform>();
+            var btnWidth = btnRect.rect.size.x * btnRect.lossyScale.x;
+            var barMoveRange = btnsSize - btnWidth;
+            var barMin = GetCenterX(btnsRect) - btnsSize / 2 + btnWidth / 2;
 
             // タブの切り替え
             foreach (var tabView in tabViews)
             {
-                var otherViews = tabViews.Where(x => x.btn != tabView.btn).ToArray();
+                var otherViews = tabViews.Where(x => x.btn != tabView.btn);
                 tabView.btn
                     .OnClickAsObservable()
                     .Subscribe(_ =>
                     {
-                        Debug.Log("マーシー");
-                        
                         // 押された要素以外のタブの非表示とボタンの色を薄くする
                         foreach (var otherView in otherViews)
                         {
                             otherView.tab.SetActive(false);
-                            otherView.icon.color = disableColor;
-                            otherView.text.color = disableColor;
+                            otherView.icon.color = DISABLE_COLOR;
+                            otherView.text.color = DISABLE_COLOR;
                         }
                         
                         // 押された要素のタブの表示とボタンの色を濃くする
                         tabView.tab.SetActive(true);
                         tabView.icon.color = Color.white;
                         tabView.text.color = Color.white;
+                        
+                        // タブラインバーのアニメーション
+                        DOTween.To(
+                            getter: () => tabLineBar.value,
+                            setter: value => tabLineBar.value = value,
+                            endValue: (GetCenterX(tabView.text.rectTransform) - barMin) / barMoveRange,
+                            duration: animDuration
+                        );
                     })
                     .AddTo(this);
             }
-            
-            //Todo:タブバーのアニメーション
+
+            // 指定のUIの中央のｘ座標を取得
+            float GetCenterX(RectTransform targetRect)
+            {
+                var position = targetRect.position.x;
+
+                if (targetRect.pivot != Vector2.one / 2)
+                {
+                    var width = targetRect.rect.size.x;
+                    position -= Mathf.Lerp(-width / 2f, width / 2f, targetRect.pivot.x) * targetRect.lossyScale.x;
+                }
+
+                return position;
+            }
         }
     }
 }
