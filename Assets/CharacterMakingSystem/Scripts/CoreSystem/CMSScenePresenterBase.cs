403516e640e2b781c104220539f1041abce40e93
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2023-01-31 10:01:32 +0900

    ScrollViewにボタン追加・そろそろ組み込み部分作る

---
+++ Assets/CharacterMakingSystem/Scripts/CoreSystem/CMSScenePresenterBase.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CharacterMakingSystem.CoreSystem
+{
+    /// <summary>
+    /// 各シーンのプレセンターのベースクラス
+    /// </summary>
+    public abstract class CMSScenePresenterBase : MonoBehaviour
+    {
+        void SelectSetColor(Image target, Image[] images)
+        {
+            
+        }
+    }
+}
